965e449e3733ace07636946296b226bda31e4f21
Bhavana Nagabandi <Bhavana.Nagabandi@walmartlabs.com>
2018-06-08 17:29:07 -0700

    first reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Head First JavaScript'},
+    {title: 'The immortals of Meluha'},
+    {title: 'The Road not taken'},
+    {title: 'To be declared!'},
+  ]
+}
