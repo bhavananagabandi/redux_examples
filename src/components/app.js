2a515100cc08672773e5b51697dc49ef0d28feda
Bhavana Nagabandi <Bhavana.Nagabandi@walmartlabs.com>
2018-06-20 15:51:24 -0700

    finished book selection app

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import BookList from '../containers/book-list'
+import BookList from '../containers/book-list';
+import ActiveBook from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <ActiveBook />
       </div>
     );
   }
